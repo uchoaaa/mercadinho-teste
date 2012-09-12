b57b58e42b6c979d3e37d8ac9ccf3126e9ed8f58
Rafael Uchôa <rafael@overmediacast.com>
2012-09-12 19:17:14 -0300

    Relacionando Client com Product

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912221131) do
+ActiveRecord::Schema.define(:version => 20120912221423) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20120912221131) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category"
+    t.integer  "client_id"
   end
 
 end
