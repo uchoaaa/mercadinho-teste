7bf35349d38ac2b88aba98093c6ca4310a1d2436
Rafael Uchôa <rafael@overmediacast.com>
2012-09-13 13:31:10 -0300

    Calculando o preco da Sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,18 @@
 class Sale < ActiveRecord::Base
   belongs_to :client
   has_and_belongs_to_many :products
+
+  def price
+    # Pulo do gato Ruby :)
+    # products.inject(0) {|sum, product| sum += product.price }.to_f
+
+    sum = 0
+    products.each do |product|
+      sum += product.price
+    end
+
+    #a ultima linha sempre é o retorno da função. Não precisa declara explicitamente com 'return'.
+    #to_f é para transformar o valor em BigDecimal para Float.
+    sum.to_f
+  end
 end
